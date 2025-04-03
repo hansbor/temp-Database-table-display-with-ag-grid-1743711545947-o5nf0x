4da82a00301c5f6a9d38bdadab6f4f44455fabac
hansbor <142583537+hansbor@users.noreply.github.com>
2025-04-03 22:19:32 +0200

    Copy vite.config.ts from Database-table-display-with-ag-grid

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config(); // Load environment variables from .env file
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+  ],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+})
